9135dae3a3982035efd3eea1792ada308743af32
Kira Roberts, Loewy Malkovich and Molly Stambaugh <molly@>
2019-08-15 13:24:38 -0700

    add radio box and show/hide function to scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 // link for under-age users https://www.vote411.org/
 // link for voters https://sos.oregon.gov/voting/Pages/registration.aspx?lang=en
 // create list for upcoming elections
-//
 
 
 
 $(document).ready(function(){
 
   $("form#ageQ").submit(function(event) {
-    event.preventDefault();
     var age = $("input:radio[name=age]:checked").val();
+    event.preventDefault();
 
     if (age >= 18) {
-      $("#voter").addClass("voterStyle");
+      $("#voter").show();
+      $("#minor").hide();
     } else {
-      $("#minor").removeClass();
-      $("#minor").addClass("minorStyle");
+      $("#voter").hide();
+      $("#minor").show();
     }
   });
 });
